d072443abed3afb438f00d17d067b3b6fdefafc1
Vivek Verma <vivekvermaiit@gmail.com>
2025-08-19 14:15:45 -0400

    search with options of english only or Tlingit only

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -4,16 +4,17 @@ class LinesController < ApplicationController
   def index
     @query_text = params[:q]
     @query_regex = params[:regex]
+    scope = params[:scope] || "both"
 
     @lines = []
     @lines = Line.includes(sentence: :corpus_entry)
-                   .search_text(@query_text)
+                   .search_text(@query_text, scope)
                    .order(:line_number)
                    .page(params[:page])
                    .per(20) if @query_text.present?
 
     @lines = Line.includes(sentence: :corpus_entry)
-                 .search_regex(@query_regex)
+                 .search_regex(@query_regex, scope)
                  .order(:line_number)
                  .page(params[:page])
                  .per(20) if @query_regex.present?
--- app/models/line.rb
+++ app/models/line.rb
@@ -2,12 +2,25 @@ class Line < ApplicationRecord
   belongs_to :sentence
   has_one :corpus_entry, through: :sentence
 
-  scope :search_text, ->(term) {
-    where("line_tlingit LIKE ? OR line_english LIKE ?", "%#{term}%", "%#{term}%")
+  scope :search_text, ->(term, scope) {
+    case scope
+    when "english"
+      where("line_english LIKE ?", "%#{term}%")
+    when "tlingit"
+      where("line_tlingit LIKE ?", "%#{term}%")
+    else # both
+      where("line_tlingit LIKE ? OR line_english LIKE ?", "%#{term}%", "%#{term}%")
+    end
   }
 
-  scope :search_regex, ->(term) {
-    where("line_tlingit REGEXP ? OR line_english REGEXP ?", "#{term}", "#{term}")
+  scope :search_regex, ->(term, scope) {
+    case scope
+    when "english"
+      where("line_english REGEXP ?", term)
+    when "tlingit"
+      where("line_tlingit REGEXP ?", term)
+    else # both
+      where("line_tlingit REGEXP ? OR line_english REGEXP ?", term, term)
+    end
   }
-
 end
